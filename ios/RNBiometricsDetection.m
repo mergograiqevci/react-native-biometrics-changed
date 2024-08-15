a5813edb89965c0da0671304627b30bfa2a2bcf5
Mergim Graiqevci <mergim.graiqevci@ndbit.net>
2024-08-15 15:09:44 +0200

    updated ios files

--- ios/RNBiometricsDetection.m
+++ ios/RNBiometricsDetection.m
@@ -1,15 +1,12 @@
-//
-//  RNBiometricsDetection.m
-//  bezahlkarte
-//
-//  Created by Mergim Graiqevci on 12.8.24.
-//
-
+// RNBiometricsDetection.m
 #import <Foundation/Foundation.h>
-#import "React/RCTBridgeModule.h"
+#import <React/RCTBridgeModule.h>
+
 @interface RCT_EXTERN_MODULE(RNBiometricsDetection, NSObject)
-  RCT_EXTERN_METHOD(
-    getFingerprintInfo: (RCTPromiseResolveBlock) resolve
-    rejecter: (RCTPromiseRejectBlock) reject
-  )
+
+RCT_EXTERN_METHOD(
+  getFingerprintInfo: (RCTPromiseResolveBlock)resolve
+  rejecter: (RCTPromiseRejectBlock)reject
+)
+
 @end
