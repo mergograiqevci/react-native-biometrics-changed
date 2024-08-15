1274c3b7812536c317ae6ab346a990399e1c0d6e
Mergim Graiqevci <mergim.graiqevci@ndbit.net>
2024-08-15 13:48:17 +0200

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { RNBiometricsDetection } = NativeModules;
+
+export default RNBiometricsDetection;
---
+++ ios/RNBiometricsDetection.m
@@ -0,0 +1,13 @@
+
+#import "RNBiometricsDetection.h"
+
+@implementation RNBiometricsDetection
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+@end
+  
