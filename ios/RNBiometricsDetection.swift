108a007804e796b6b42915eed0428f9da9b5fbd8
Mergim Graiqevci <mergim.graiqevci@ndbit.net>
2024-08-16 14:38:21 +0200

    improvements for biometrics detection

--- ios/RNBiometricsDetection.swift
+++ ios/RNBiometricsDetection.swift
@@ -8,26 +8,28 @@ import LocalAuthentication
   ) -> Void {
     let context = LAContext()
     
-    if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) {
-        // Retrieve the previously stored data from Keychain
-        if let storedData = KeychainHelper.load(key: "fingerprintID"),
-          let storedString = String(data: storedData, encoding: .utf8) {
-          KeychainHelper.save(key: "fingerprintID", data: bData)
-          // Compare the decoded string with the stored data
-          if decodedString == storedString {
-              print("Fingerprint ID matches.")
-              resolve(false)
-          } else {
-              print("Fingerprint ID does not match.")
-              resolve(true)
+      if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) {
+          if let domainState = context.evaluatedPolicyDomainState {
+              let bData = domainState.base64EncodedData()
+              if let decodedString = String(data: bData, encoding: .utf8) {
+                  // Retrieve the previously stored data from Keychain
+                  if let storedData = KeychainHelper.load(key: "fingerprintID"),
+                     let storedString = String(data: storedData, encoding: .utf8) {
+                      KeychainHelper.save(key: "fingerprintID", data: bData)
+                      // Compare the decoded string with the stored data
+                      if decodedString == storedString {
+                          resolve(false)
+                      } else {
+                          resolve(true)
+                      }
+                  } else {
+                      KeychainHelper.save(key: "fingerprintID", data: bData)
+                      resolve(false)
+                  }
+              }
           }
-        } else {
-            KeychainHelper.save(key: "fingerprintID", data: bData)
-            resolve(false)
-        }
-    }
+      }
     else {
-        print("Cannot evaulate")
         reject("0", "Cannot evaulate", nil)
     }
   }
